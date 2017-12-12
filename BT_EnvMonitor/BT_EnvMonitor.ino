13f87c32b983e3139de26447f06e65d41e6f81ca
smeualex <smeualex@gmail.com>
2017-12-12 12:42:07 +0200

    Added description Refactored the timed function call

--- BT_EnvMonitor/BT_EnvMonitor.ino
+++ BT_EnvMonitor/BT_EnvMonitor.ino
@@ -2,19 +2,30 @@
  Name:		BT_EnvMonitor.ino
  Created:	12/12/2017 12:05:55 PM
  Author:	asm
+
+ Description:   The main purpose of this project is to provide local environment data
+                via a 2x16 LCD and via bluetooth to any connected devices.
+
+ Components:    
+    1602    LCD with I2C Interface
+    DS1307  RTC I2C module
+    HC-05   Bluetooth module
+    BME680  Enviroment sensor (temp, humidity, pressure and air quality)
 */
 
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+// comment this line to turn Serial logging off
+#define _DEBUGGING_ON_
+
 #include <Wire.h>
-#include "RTClib.h"
 #include <LiquidCrystal_I2C.h>
+#include "RTClib.h"
 #include "SoftwareSerial.h"
 #include "Adafruit_BME680.h"
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define BME680_I2C_ADDR       0x76
 
-Adafruit_BME680 bme; // I2C
+Adafruit_BME680 bme;
 
 #define HW_BAUDRATE         115200
 #define BLUETOOTH_BAUDRATE    9600 
@@ -30,17 +41,21 @@ LiquidCrystal_I2C lcd(LCD_I2C_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 inline void lcd1() { lcd.setCursor(0, 0); }
 inline void lcd2() { lcd.setCursor(0, 1); }
 
-uint32_t RTC_LastReading;
+uint32_t RTC_BME680_LastReading;
 uint32_t RTC_CurrentReading;
 
-const unsigned int BME_READ_INTERVAL = 5;
+const uint32_t BME_READ_INTERVAL = 5;
 const unsigned int LCD_SWITCH_INTERVAL = 10;
 
 const char* months[12] = { "Ian", "Feb", "Mar", "Apr", "Mai", "Iun", "Iul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
 SoftwareSerial BTSerial(10, 11);
+
+/* Bluetooth commands   */
 #define BT_GET_DATE     '1'
 #define BT_GET_ENVDATA  '2'
+
+void _callFunctionOnTimer(void(*fun)(void), const uint32_t timerValue, uint32_t *timer);
 
 void printStartupMessage()
 {
@@ -103,71 +118,42 @@ void setup()
 
     RTC_CurrentReading = rtc.now().unixtime();
     // set last reading in the past to force the display of data
-    RTC_LastReading = RTC_CurrentReading - BME_READ_INTERVAL;
+    RTC_BME680_LastReading = RTC_CurrentReading - BME_READ_INTERVAL;
 }
 
 void loop()
 {
     dt = rtc.now();
+    RTC_CurrentReading = dt.unixtime();
     // always display the date
     displayDate();
-    // update the temp data on timer
-    RTC_CurrentReading = dt.unixtime();
-    if (RTC_CurrentReading - RTC_LastReading >= BME_READ_INTERVAL)
-    {
-        BME680_DisplayData();
-        RTC_LastReading = RTC_CurrentReading;
-    }
-
-    if (BTSerial.available())
-    {
-        char btData = BTSerial.read();
-
-        if (btData == BT_GET_DATE)
-        {
-            DateTime now = rtc.now();
-
-            BTSerial.print("DATE:");
-            BTSerial.print("h="); BTSerial.print(now.hour()); BTSerial.print(";");
-            BTSerial.print("m="); BTSerial.print(now.minute()); BTSerial.print(";");
-            BTSerial.print("s="); BTSerial.print(now.second()); BTSerial.print(";");
-
-            BTSerial.print("y="); BTSerial.print(now.year()); BTSerial.print(";");
-            BTSerial.print("m="); BTSerial.print(now.month()); BTSerial.print(";");
-            BTSerial.print("d="); BTSerial.print(now.day()); BTSerial.print(";");
-
-            BTSerial.println("#");
-        }
-        else if (btData == BT_GET_ENVDATA)
-        {
-            if (!bme.performReading())
-            {
-                Serial.println("Failed to perform reading :(");
-                return;
-            }
-
-            /*
-            BTSerial.print("ENV:");
-            BTSerial.print(bme.temperature); BTSerial.print(";");
-            BTSerial.print(bme.humidity);    BTSerial.print(";");
-            BTSerial.print(bme.pressure);    BTSerial.print(";");
-            BTSerial.println("#");
-            */
-
-            char  buf[128];
-            int   hum_DEC = static_cast<int> (bme.humidity * 100);
-            int   temp_DEC = static_cast<int> (bme.temperature * 100);
-            float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
-
-            BTSerial.print("Temperature: "); BTSerial.print(bme.temperature);    BTSerial.println();
-            BTSerial.print("Humidity:    "); BTSerial.print(bme.humidity);       BTSerial.println();
-            BTSerial.print("Pressure:    "); BTSerial.print(bme.pressure / 100.0); BTSerial.println();
-            BTSerial.print("Altitude:    "); BTSerial.print(altitude);
-            BTSerial.println("#");
-        }
-    }
+    // update the env. data on timer
+    _callFunctionOnTimer(BME680_DisplayData, BME_READ_INTERVAL, &RTC_BME680_LastReading);
+    // check for BT
+    BT_CheckForData();
 }
 
+/*
+ Utility function to call another function after certain amount of time has passed
+ Time is checked with an RTC rather than millis() so we have 1s resolution
+
+ Params:
+    void(*fun)(void) - function to call 
+                     - usually a display update function
+
+     timerValue      - timer value in seconds
+
+     timer           - the timer to store the specific readings
+
+*/
+void _callFunctionOnTimer(void(*fun)(void), const uint32_t timerValue, uint32_t *timer)
+{
+    if (RTC_CurrentReading - (*timer) >= timerValue)
+    {
+        fun();
+        *timer = RTC_CurrentReading;
+    }
+}
 // LCD Data Format
 // |----------------|
 // |09:56   07-12-17| HH:MM   DD-MM-YY
@@ -208,6 +194,8 @@ void BME680_DisplayData()
         temp_DEC / 100, roundTo1Decimal(temp_DEC % 100),
         hum_DEC / 100, roundTo1Decimal(hum_DEC % 100));
     lcd.print(row2);
+
+#ifdef _DEBUGGING_ON_
     // -------------------------------------------------------//
     {
         char tmp[20 + 1];
@@ -239,4 +227,44 @@ void BME680_DisplayData()
         Serial.println(" m");
     }
     // -------------------------------------------------------//
+#endif
+}
+
+void BT_CheckForData()
+{
+    if (BTSerial.available())
+    {
+        char btData = BTSerial.read();
+
+        if (btData == BT_GET_DATE)
+        {
+            DateTime now = rtc.now();
+
+            BTSerial.print("DATE:");
+            BTSerial.print("h="); BTSerial.print(now.hour()); BTSerial.print(";");
+            BTSerial.print("m="); BTSerial.print(now.minute()); BTSerial.print(";");
+            BTSerial.print("s="); BTSerial.print(now.second()); BTSerial.print(";");
+
+            BTSerial.print("y="); BTSerial.print(now.year()); BTSerial.print(";");
+            BTSerial.print("m="); BTSerial.print(now.month()); BTSerial.print(";");
+            BTSerial.print("d="); BTSerial.print(now.day()); BTSerial.print(";");
+
+            BTSerial.println("#");
+        }
+        else if (btData == BT_GET_ENVDATA)
+        {
+            if (!bme.performReading())
+            {
+                Serial.println("Failed to perform reading :(");
+                return;
+            }
+            float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+            BTSerial.print("Temperature: "); BTSerial.print(bme.temperature);    BTSerial.println();
+            BTSerial.print("Humidity:    "); BTSerial.print(bme.humidity);       BTSerial.println();
+            BTSerial.print("Pressure:    "); BTSerial.print(bme.pressure / 100.0); BTSerial.println();
+            BTSerial.print("Altitude:    "); BTSerial.print(altitude);
+            BTSerial.println("#");
+        }
+    }
 }
