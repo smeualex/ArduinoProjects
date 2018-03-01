8f185bd938af0efeb69402b831bfb61689325467
smeualex <smeualex@gmail.com>
2018-03-01 17:21:15 +0200

    Added timed action with number of executions and a post exec callback. Improved the timed action constructor logic

--- snake3/TimedAction.cpp
+++ snake3/TimedAction.cpp
@@ -1,24 +1,31 @@
 #include "TimedAction.h"
 
-TimedAction::TimedAction(unsigned long intervl,void (*function)())
+TimedAction::TimedAction(unsigned long intervl,void (*function)(), void(*postFunction)(), unsigned long n)
 {
     active = true;
 	previous = 0;
 	interval = intervl;
 	execute = function;
+    nExecutionsBeforeStop = n;  
+    postExecute = postFunction;
+    execCounter = 0;
 }
 
-TimedAction::TimedAction(unsigned long prev,unsigned long intervl,void (*function)())
+TimedAction::TimedAction(unsigned long prev,unsigned long intervl,void (*function)(), void(*postFunction)(), unsigned long n)
 {
-    active = true;
 	previous = prev;
-	interval = intervl;
-	execute = function;
+    TimedAction(intervl, function, postFunction, n);
 }
 
 void TimedAction::reset()
 {
     previous = millis();
+    if (postExecute != NULL && nExecutionsBeforeStop != EXECUTE_INDEFINETLY && execCounter >= nExecutionsBeforeStop)
+    {
+        execCounter = 0;
+        if (!isActive())
+            enable();
+    }
 }
 
 void TimedAction::disable()
@@ -31,12 +38,42 @@ void TimedAction::enable()
 	active = true;
 }
 
-void TimedAction::check(){
-  if ( active && (millis()-previous >= interval) ) 
-  {
-    previous = millis();
-    execute();
-  }
+
+void TimedAction::__execute()
+{ 
+    if (millis() - previous >= interval)
+    {
+        previous = millis();
+        ++execCounter;
+        execute();
+    }
+}
+
+void TimedAction::check()
+{
+    if (!isActive())
+        return;
+
+    if (nExecutionsBeforeStop == EXECUTE_INDEFINETLY)
+    {
+        __execute();
+    }
+    else
+    {
+        if (execCounter <= nExecutionsBeforeStop)
+        {
+            __execute();
+            if (execCounter == nExecutionsBeforeStop)
+            {
+                execCounter++;
+                postExecute();
+            }
+        }
+        else
+        {
+            disable();
+        }
+    }
 }
 
 void TimedAction::setInterval( unsigned long intervl)
