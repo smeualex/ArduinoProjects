378cc87cc61ef900aa6cfb7797372343672b1719
smeul <smeualex@gmail.com>
2018-03-03 12:56:15 +0200

    Reorganize the header.. donno why...

--- snake3/Point.h
+++ snake3/Point.h
@@ -5,34 +5,12 @@
 class Point 
 {
 public:
-    Point()
-    {
-        x = 255;
-        y = 255;
-    }
-
-    Point(byte pX, byte pY)
-    {
-        x = pX;
-        y = pY;
-    }
-
-    Point& operator=(Point p)
-    {
-        x = p.x;
-        y = p.y;
-        return *this;
-    }
-
-    bool operator==(const Point &p) const
-    {
-        return ((x == p.x) && (y == p.y) );
-    }
-
-    bool operator!=(const Point &p) const
-    {
-        return !(*this == p);
-    }
+    Point()                                 { x = 255; y = 255;                   }
+    Point(byte pX, byte pY)                 { x = pX; y = pY;                     }
+    
+    Point& operator=(Point p)               { x = p.x; y = p.y; return *this;     }
+    bool operator==(const Point &p) const   { return ((x == p.x) && (y == p.y) ); }
+    bool operator!=(const Point &p) const   { return !(*this == p);               }
 
     byte x;
     byte y;
